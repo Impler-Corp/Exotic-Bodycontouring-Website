40fd6ad975f76fe4b537a6f0690bd9609166b806
Snowin <20103104@hit.edu.in>
2022-07-09 00:02:03 +0530

    added basic styles and structures

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,8 @@
-console.log("I'm script file");
+const open_menu = document.querySelector(".btn-open__menu");
+const close_menu = document.querySelector(".btn-close__menu");
+const links = document.querySelector(".links");
+
+open_menu.addEventListener("click", () => {
+  console.log("clicked!!");
+  links.style.transform = "translateX(0%)";
+});
