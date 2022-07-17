70014cb2b59f9db0d5f8ff14bb26010b02b7886a
devsnowin <20103104@hit.edu.in>
2022-07-17 15:46:18 +0530

    adjusting responsiveness

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -3,3 +3,19 @@ window.addEventListener("scroll", () => {
   const primaryHeader = document.querySelector("header");
   primaryHeader.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
