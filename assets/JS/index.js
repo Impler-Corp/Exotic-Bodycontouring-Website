25b9471f828e07357e48ea69d0510602e4e32572
Ruban Gino Singh <rubangino22@gmail.com>
2022-07-23 15:25:03 +0530

    prefinal

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -19,3 +19,50 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// Contact Form with AJAX
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      alert("Thanks for the submission!!");
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          alert("Oops! There was a problem submitting your form");
+        }
+      })
+    }
+  }).catch(error => {
+    alert("Oops! There was a problem submitting your form");
+  });
+}
+form.addEventListener("submit", handleSubmit)
+
+// Nav
+const navItems = document.querySelector(".navigation_links")
+const navLinks = document.querySelectorAll(".nav-link")
+const btnChecked = document.querySelector(".checkbtn")
+
+btnChecked.addEventListener("click", () => {
+  navItems.classList.toggle("toggleMenu")
+})
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navItems.classList.toggle("toggleMenu")
+  })
+})
